c97b94fe667ac88c976a24c592481e873c207404
Your Name <you@example.com>
2021-07-14 23:50:58 +0630

    add recycle view to home fragment

--- app/src/main/java/com/example/myittaroostockinventorymanger/MainActivity.java
+++ app/src/main/java/com/example/myittaroostockinventorymanger/MainActivity.java
@@ -3,14 +3,20 @@ package com.example.myittaroostockinventorymanger;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.example.myittaroostockinventorymanger.fragments.AboutFragment;
 import com.example.myittaroostockinventorymanger.fragments.DashboardFragment;
 import com.example.myittaroostockinventorymanger.fragments.HomeFragment;
 import com.example.myittaroostockinventorymanger.fragments.TransactionFragment;
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
 import butterknife.BindView;
@@ -24,7 +30,16 @@ public class MainActivity extends AppCompatActivity {
     NavigationView navView;
     @BindView(R.id.tool_bar)
     MaterialToolbar toolbar;
-
+    @BindView(R.id.fab_menu)
+    FloatingActionButton fabMenu;
+    @BindView(R.id.fab_in)
+    FloatingActionButton fabIN;
+    @BindView(R.id.fab_out)
+    FloatingActionButton fabOut;
+    @BindView(R.id.fab_scan)
+    FloatingActionButton fabScan;
+    private Animation rotateOpen, rotateClose, fromBottom, toBottom;
+    private boolean isOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +47,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+
+        //for expandable fab animation
+        rotateOpen = AnimationUtils.loadAnimation(this, R.anim.rotate_open_animation);
+        rotateClose = AnimationUtils.loadAnimation(this, R.anim.rotate_close_animation);
+        fromBottom = AnimationUtils.loadAnimation(this, R.anim.from_bottom_animation);
+        toBottom = AnimationUtils.loadAnimation(this, R.anim.to_bottom_animation);
 
         HomeFragment hf = new HomeFragment();
         createFragment(hf);
@@ -42,6 +63,10 @@ public class MainActivity extends AppCompatActivity {
 
 
         navDrawerOnItemSelected();
+
+        fabMenu.setOnClickListener(v -> {
+            onFabMenuClicked();
+        });
     }
 
     //menu item selected change fragment
@@ -51,23 +76,28 @@ public class MainActivity extends AppCompatActivity {
         navView.setNavigationItemSelectedListener(item -> {
 
             int id = item.getItemId();
+            navView.setCheckedItem(item);
 
             switch (id) {
                 case R.id.nav_home:
                     HomeFragment hf = new HomeFragment();
                     createFragment(hf);
+                    fabMenu.show();
                     break;
                 case R.id.nav_transaction:
                     TransactionFragment tf = new TransactionFragment();
                     createFragment(tf);
+                    fabMenu.show();
                     break;
                 case R.id.nav_dashboard:
                     DashboardFragment df = new DashboardFragment();
                     createFragment(df);
+                    fabMenu.hide();
                     break;
                 case R.id.nav_about:
                     AboutFragment af = new AboutFragment();
                     createFragment(af);
+                    fabMenu.hide();
                     break;
             }
             drawerLayout.closeDrawer(navView);
@@ -82,5 +112,49 @@ public class MainActivity extends AppCompatActivity {
                 .beginTransaction()
                 .add(R.id.fragment_view, fg)
                 .commit();
+    }
+
+    private void onFabMenuClicked() {
+
+        setVisibility();
+        setAnimation();
+        changeFabMenuIcon();
+        isOpen = !isOpen;
+    }
+
+    //animation for floating action button
+    private void setAnimation() {
+        if (!isOpen) {
+            fabIN.setAnimation(fromBottom);
+            fabOut.setAnimation(fromBottom);
+            fabScan.setAnimation(fromBottom);
+            fabMenu.setAnimation(rotateOpen);
+        } else {
+            fabIN.setAnimation(toBottom);
+            fabOut.setAnimation(toBottom);
+            fabScan.setAnimation(toBottom);
+            fabMenu.setAnimation(rotateClose);
+        }
+    }
+
+    //visibility for multi floating action button
+    private void setVisibility() {
+        if (!isOpen) {
+            fabIN.setVisibility(View.VISIBLE);
+            fabOut.setVisibility(View.VISIBLE);
+            fabScan.setVisibility(View.VISIBLE);
+        } else {
+            fabIN.setVisibility(View.INVISIBLE);
+            fabOut.setVisibility(View.INVISIBLE);
+            fabScan.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void changeFabMenuIcon() {
+        if (!isOpen) {
+            fabMenu.setImageResource(R.drawable.ic_add);
+        } else {
+            fabMenu.setImageResource(R.drawable.ic_menu);
+        }
     }
 }
--- app/src/main/java/com/example/myittaroostockinventorymanger/fragments/HomeFragment.java
+++ app/src/main/java/com/example/myittaroostockinventorymanger/fragments/HomeFragment.java
@@ -1,22 +1,48 @@
 package com.example.myittaroostockinventorymanger.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.RotateAnimation;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myittaroostockinventorymanger.R;
+import com.example.myittaroostockinventorymanger.adapters.StockListRecycleViewAdapter;
+import com.example.myittaroostockinventorymanger.util.VerticalSpaceItemDecoration;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class HomeFragment extends Fragment {
+
+    @BindView(R.id.recy_stock_list)
+    RecyclerView recyStockList;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-      View view = inflater.inflate(R.layout.fragment_home,container,false);
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this, view);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        StockListRecycleViewAdapter stockListAdapter = new StockListRecycleViewAdapter();
+        recyStockList.setAdapter(stockListAdapter);
+        recyStockList.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyStockList.addItemDecoration(new VerticalSpaceItemDecoration(24));
     }
 }
